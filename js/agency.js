1f8b1eee54bf5f8783aa766b448ff5a69962c3a1
Chris <chris@Chriss-MacBook-Pro-2.local>
2014-08-14 17:42:05 -0400

    First Upload Personal Website

---
+++ js/agency.js
@@ -0,0 +1,49 @@
+/*!
+ * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+    if ($(window).width() < 768)
+    	$('#click').click();
+});
+
+$(document).ready(function(){
+	$('#typing').typed({
+		strings: [" Hello World!^200"],
+		typeSpeed: 50,
+		cursorOn: false,
+		callback: function(){
+			$('#typing2').typed({
+				strings: ["^750I am a student.", "I am a hacker.", "I am a developer.", "I am a website designer.", "I am a computer engineer.^200"],
+				stopNum: 6,
+				backDelay: 500,
+				cursorOn: false,
+				callback: function(){
+					$('.fade_in_txt').delay(100).fadeIn(4000);
+				}
+			});
+		}
+	});
+	
+});
+
---
+++ js/cbpAnimatedHeader.js
@@ -0,0 +1,49 @@
+/**
+ * cbpAnimatedHeader.js v1.0.0
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2013, Codrops
+ * http://www.codrops.com
+ */
+var cbpAnimatedHeader = (function() {
+
+	var docElem = document.documentElement,
+		header = document.querySelector( '.navbar-default' ),
+		didScroll = false,
+		// Use this to change when the header toggles
+		// 565 for toronto_at_night_black_and_white_edit_by_falcon912-d5nkmrv
+		// changeHeaderOn = 565;
+		changeHeaderOn=$(window).height()-200;
+
+	function init() {
+		window.addEventListener( 'scroll', function( event ) {
+			if( !didScroll ) {
+				didScroll = true;
+				setTimeout( scrollPage, 250 );
+			}
+		}, false );
+	}
+
+	function scrollPage() {
+		var sy = scrollY();
+		if ( sy >= changeHeaderOn ) {
+			classie.add( header, 'navbar-shrink' );
+			// $('.navbar-default').css("background-color", "rgba(255,255,255,0.8)");
+		}
+		else {
+			classie.remove( header, 'navbar-shrink' );
+			// $('.navbar-default').css("background-color", "transparent");
+		}
+		didScroll = false;
+	}
+
+	function scrollY() {
+		return window.pageYOffset || docElem.scrollTop;
+	}
+
+	init();
+
+})();
