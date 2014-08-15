2aef96b2aa57ec7817dd893cd7d9ee8e040e3f3f
Chris <chris.wong0929@gmail.com>
2014-08-15 15:26:01 -0400

    sendgrid

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -9,18 +9,32 @@ if(empty($_POST['name'])      ||
    echo "No arguments Provided!";
    return false;
    }
-   
+
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
+
+
+
+
+$sendgrid = new SendGrid('app28496374@heroku.com', 'gq9tox4l');
+$message = new SendGrid\Email();
+
+$message->addTo('chris.wong0929@gmail.com')->
+          setFrom('me@bar.com')->
+          setSubject('Subject goes here')->
+          setText('Hello World!')->
+          setHtml('<strong>Hello World!</strong>');
+$response = $sendgrid->send($message);
+
    
 // Create the email and send the message
-$to = 'chris.wong0929@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-$retval = mail($to,$email_subject,$email_body,$headers);
-return $retval;         
+// $to = 'chris.wong0929@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+// $email_subject = "Website Contact Form:  $name";
+// $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+// $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+// $headers .= "Reply-To: $email_address";   
+// mail($to,$email_subject,$email_body,$headers);
+return true;         
 ?>
